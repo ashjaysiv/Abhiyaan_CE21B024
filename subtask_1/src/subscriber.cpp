90f4485176284fef44d9d4284152a37a4d4287d8
ashjaysiv <96778394+ashjaysiv@users.noreply.github.com>
2023-03-31 05:51:07 +0530

    Update subscriber.cpp

--- subtask_1/src/subscriber.cpp
+++ subtask_1/src/subscriber.cpp
@@ -5,17 +5,23 @@ std_msgs::String message;
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
-	ROS_INFO("Team Abhiyaan SOFTWARE_APLICATION");
+	//message received from the publisher node is saved ("No.1 CFI TEAM")
 	message.data  = msg->data.c_str();
 
 }
 
 int main(int argc, char **argv)
 {
+	//initialize
 	ros::init(argc, argv, "subscriber");
+	
+	//create node variable
 	ros::NodeHandle n;
+	
+	//create subscriber node - /chatter
 	ros::Subscriber sub = n.subscribe("chatter", 1, chatterCallback);
 	
+	// create a publisher node - /absolute_truth
 	ros::Publisher chatter_pub = n.advertise<std_msgs::String>("absolute_truth", 1);
 
 	ros::Rate loop_rate(10);	
@@ -27,7 +33,8 @@ int main(int argc, char **argv)
 		std::stringstream ss;
 		ss<<message<<"ABHIYAAN";
 		msg1.data = ss.str();
-
+		
+		//publisher publishes the msg "NO.1 CFI TEAM" + "ABHIYAAN" to the node "absolute_truth"
 		ROS_INFO("%s", msg1.data.c_str());
 
 		chatter_pub.publish(msg1);
